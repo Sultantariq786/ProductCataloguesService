04df71c992cb957c9d58ee2da6f2c3fe19c404bb
Sultantariq786 <newtechtariq@gmail.com>
2019-02-03 15:36:38 +0530

    change product model

--- src/main/java/com/angular/productcatalogues/model/Product.java
+++ src/main/java/com/angular/productcatalogues/model/Product.java
@@ -14,14 +14,31 @@ public class Product {
 	private String productName;
 	@Field("product_desc")
 	private String productDesc;
+	@Field("product_category")
+	private String productCategory;
+	@Field("product_img")
+	private String productImg;
+	@Field("product_alt")
+	private String productAlt;
+	@Field("product_qty")
+	private Integer productQty;
+	@Field("product_price")
+	private Integer productPrice;
+	
 	
 	public Product(){
 		
 	}
-	public Product(Integer productId, String productName, String productDesc){
+	public Product(Integer productId, String productName, String productDesc,
+			String productCategory, String productImg,String productAlt, Integer productQty,Integer productPrice){
 		this.productId = productId;
 		this.productName = productName;
 		this.productDesc = productDesc;
+		this.productCategory = productCategory;
+		this.productImg = productImg;
+		this.productAlt = productAlt;
+		this.productQty = productQty;
+		this.productPrice = productQty;
 	}
 
 	public Integer getProductId() {
@@ -48,9 +65,43 @@ public class Product {
 		this.productDesc = productDesc;
 	}
 
+	public String getProductCategory() {
+		return productCategory;
+	}
+	public void setProductCategory(String productCategory) {
+		this.productCategory = productCategory;
+	}
+	public String getProductImg() {
+		return productImg;
+	}
+	public void setProductImg(String productImg) {
+		this.productImg = productImg;
+	}
+	public String getProductAlt() {
+		return productAlt;
+	}
+	public void setProductAlt(String productAlt) {
+		this.productAlt = productAlt;
+	}
+	public Integer getProductQty() {
+		return productQty;
+	}
+	public void setProductQty(Integer productQty) {
+		this.productQty = productQty;
+	}
+	public Integer getProductPrice() {
+		return productPrice;
+	}
+	public void setProductPrice(Integer productPrice) {
+		this.productPrice = productPrice;
+	}
 	@Override
 	public String toString() {
 		return "Product [productId=" + productId + ", productName="
-				+ productName + ", productDesc=" + productDesc + "]";
+				+ productName + ", productDesc=" + productDesc
+				+ ", productCategory=" + productCategory + ", productImg="
+				+ productImg + ", productAlt=" + productAlt + ", productQty="
+				+ productQty + ", productPrice=" + productPrice + "]";
 	}
+	
 }
